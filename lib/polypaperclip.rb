4d59daf92bd401baf95cdb00a918f8ad7874b7ec
Dan Pickett <dpickett@enlightsolutions.com>
2011-05-29 15:01:22 -0400

    reorg so poly initialization only happens once

--- lib/polypaperclip.rb
+++ lib/polypaperclip.rb
@@ -11,20 +11,16 @@ module Polypaperclip
     def polypaperclip_definitions
       read_inheritable_attribute(:polypaperclip_definitions)
     end
-
+    
     def has_attachment(name, options = {})
+      initialize_polypaperclip
+      
+      polypaperclip_definitions[name] = {:validations => []}.merge(options)
+      
       has_one "#{name}_attachment",
         :as => :attachable,
         :dependent => :destroy,
         :class_name => "Polypaperclip::PersistedAttachment"
-        
-      has_many_attachments_association
-      
-      after_save :save_attached_files
-      before_destroy :destroy_attached_files
-      
-      write_inheritable_attribute(:polypaperclip_definitions, {}) if polypaperclip_definitions.nil?
-      polypaperclip_definitions[name] = {:validations => []}.merge(options)
       
       define_method name do |*args|
         a = paperclip_attachment_for(name)
@@ -38,18 +34,30 @@ module Polypaperclip
       define_method "#{name}?" do
         paperclip_attachment_for(name).file?
       end
-
+      
       validates_each(name) do |record, attr, value|
         attachment = record.paperclip_attachment_for(name)
         attachment.send(:flush_errors)
       end
-      
-      #sequence is important here - we have to override some paperclip stuff
-      include Paperclip::InstanceMethods
-      include InstanceMethods
     end
 
     protected
+    def initialize_polypaperclip
+      #initialize a polypaperclip model
+      if polypaperclip_definitions.nil?
+        has_many_attachments_association
+      
+        after_save :save_attached_files
+        before_destroy :destroy_attached_files
+      
+        write_inheritable_attribute(:polypaperclip_definitions, {})
+        
+        #sequence is important here - we have to override some paperclip stuff
+        include Paperclip::InstanceMethods
+        include InstanceMethods
+      end
+    end
+    
     def has_many_attachments_association
       unless self.respond_to?(:attachments)
         has_many :attachments,
