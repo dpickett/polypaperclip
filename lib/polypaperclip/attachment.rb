933d17a22efc44c4d1d5d7ff76108f4568ec9f41
Gabe Coyne <gabe@coyne-design.com>
2011-07-07 16:32:56 -0700

    remove default_options override - this broke direct PaperClip usage

--- lib/polypaperclip/attachment.rb
+++ lib/polypaperclip/attachment.rb
@@ -1,32 +1,6 @@
 module Polypaperclip
   class Attachment < Paperclip::Attachment
     
-    def self.default_options
-      #Credit http://www.jonathanspies.com/posts/6-Using-yaml-to-configure-default-options-for-Paperclip
-      return @default_options if @default_options
-
-      @default_options = {
-        :url                   => "/system/:attachment/:id/:style/:filename",
-        :path                  => ":rails_root/public:url",
-        :styles                => {},
-        :only_process          => [],
-        :processors            => [:thumbnail],
-        :convert_options       => {},
-        :default_url           => "/:attachment/:style/missing.png",
-        :default_style         => :original,
-        :storage               => :filesystem,
-        :use_timestamp         => true,
-        :use_default_time_zone => true,
-        :hash_digest           => "SHA1",
-        :hash_data             => ":class/:attachment/:id/:style/:updated_at",
-        :preserve_files        => false
-      }
-
-      if defined?(RAILS_ROOT) and File.exists?("#{RAILS_ROOT}/config/paperclip.yml")
-        @default_options.merge!(YAML.load_file("#{RAILS_ROOT}/config/paperclip.yml")[RAILS_ENV].symbolize_keys) rescue nil
-      end
-    end
-    
     def initialize(name, instance, options = {})
       super
       
